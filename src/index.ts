c9e352dda60d6e874632f34f67cbcbeba9b6da82
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-11-25 15:09:16 -0300

    feat:change cors

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,49 @@
-import { PrismaClient } from "@prisma/client"
-import bodyParser from "body-parser"
-import compression from "compression"
-import cors from "cors"
-import express from "express"
-import { createUser } from "./handlers/CreateUsers"
-import { deleteUserById } from "./handlers/DeleteUser"
-import { listUsers } from "./handlers/GetAllUsers"
-import { getUserById } from "./handlers/GetUsersById"
-import { loginUser } from "./handlers/LoginUser"
-import { updateUserById } from "./handlers/UploadUser"
-import { errorHandler } from "./middleware/midleware"
+import { PrismaClient } from "@prisma/client";
+import bodyParser from "body-parser";
+import compression from "compression";
+import express from "express";
+import cors from "cors";
+import { createUser } from "./handlers/CreateUsers";
+import { deleteUserById } from "./handlers/DeleteUser";
+import { listUsers } from "./handlers/GetAllUsers";
+import { getUserById } from "./handlers/GetUsersById";
+import { loginUser } from "./handlers/LoginUser";
+import { updateUserById } from "./handlers/UploadUser";
+import { errorHandler } from "./middleware/midleware";
 
-const prisma = new PrismaClient()
-export const app = express()
+const prisma = new PrismaClient();
+export const app = express();
 
-app.use(express.json())
-app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json())
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = ["https://mestre-do-churrasco.vercel.app"]
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(null, false)
-      }
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  }),
-)
+app.use(express.json());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(errorHandler)
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, 
+}));
+
+
+app.use(errorHandler);
 
 // Rota de teste
 app.get("/api", (req, res) => {
-  res.send("API Funcionando")
-})
+  res.send("API Funcionando");
+});
 
 // Handlers de usuários
-app.post("/api/create-users", createUser)
-app.get("/api/user/:id", getUserById)
-app.delete("/api/user/:id", deleteUserById)
-app.put("/api/user/:id", updateUserById)
-app.post("/api/login", loginUser)
-app.get("/api/users", listUsers)
+app.post("/api/create-users", createUser);
+app.get("/api/user/:id", getUserById);
+app.delete("/api/user/:id", deleteUserById);
+app.put("/api/user/:id", updateUserById);
+app.post("/api/login", loginUser);
+app.get("/api/users", listUsers);
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`servidor rodando na porta http://localhost:${PORT}/api`)
-})
+  console.log(`servidor rodando na porta http://localhost:${PORT}/api`);
+});
